dc531e49d25361cdb549539988004f7fe5d28f05
iwona <iwo.jarek@gmail.com>
2022-05-24 11:11:56 +0200

    navbar and footer

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
     <div>
-      <h1>Footer</h1>
+      <p className="text-center text-muted">Copyright ©  BlogApp 2022</p>
     </div>
     )
   }
--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,9 +1,22 @@
+import { Container, NavLink } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+
 const NavBar = () => {
-    return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
-    )
-  }
-  
-  export default NavBar;
+  return (
+    <>
+    <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+    <Container>
+      <Navbar.Brand href="/">Blog App</Navbar.Brand>
+    <Nav className="ml-auto">
+      <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+      <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+    </Nav>
+    
+    </Container>
+    </Navbar>
+    </>
+  )
+}
+
+export default NavBar;
