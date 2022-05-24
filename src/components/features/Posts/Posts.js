3605401cb5ec89ea58b96bdfa15ad46934af7386
iwona <iwo.jarek@gmail.com>
2022-05-24 15:38:56 +0200

    something wrong

--- src/components/features/Posts/Posts.js
+++
@@ -1,9 +0,0 @@
-const Posts = () => {
-    return (
-    <div>
-      <p className="text-center text-muted">Copyright ©  BlogApp 2022</p>
-    </div>
-    )
-  }
-  
-  export default Posts;
--- src/components/views/NavBar.js
+++ src/components/views/NavBar.js
@@ -5,15 +5,16 @@ import { Navbar } from "react-bootstrap";
 const NavBar = () => {
   return (
     <>
-      <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
-        <Container>
-          <Navbar.Brand href="/">Blog.app</Navbar.Brand>
-          <Nav className="ml-auto">
-            <Nav.Link as={NavLink} href="/">Home</Nav.Link>
-            <Nav.Link as={NavLink} href="/about">About</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+    <Container>
+      <Navbar.Brand href="/">Blog App</Navbar.Brand>
+    <Nav className="ml-auto">
+      <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+      <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+    </Nav>
+    
+    </Container>
+    </Navbar>
     </>
   )
 }
