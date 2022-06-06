7b6a4161f7122afbecf7fa8fdfc5e20f902f17aa
iwona <iwo.jarek@gmail.com>
2022-06-06 18:06:25 +0200

    form

--- src/components/features/PostForm.js
+++ src/components/features/PostForm.js
@@ -4,67 +4,83 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import { useForm } from "react-hook-form";
 
 
-  const PostForm = ({ action, actionText, ...props }) => {
-    const [title, setTitle] = useState(props.title || '');
-    const [author, setAuthor] = useState(props.author || '');
-    const [publishedDate, setPublishedDate] = useState(props.publishedDate || new Date());
-    const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
-    const [content, setContent] = useState(props.content || '');
+const PostForm = ({ action, actionText, ...props }) => {
+  const [title, setTitle] = useState(props.title || '');
+  const [author, setAuthor] = useState(props.author || '');
+  const [publishedDate, setPublishedDate] = useState(props.publishedDate || new Date());
+  const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
+  const [content, setContent] = useState(props.content || '');
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
 
-    const handleSubmit = e => {
-      e.preventDefault();
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
+
+  const handleSubmit = () => {
+    setContentError(!content)
+    setDateError(!publishedDate)
+    if (content && publishedDate) {
       action({ title, author, publishedDate, shortDescription, content });
-    };
+    }
+  };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3 col-4" controlId="formTitle">
+    <Form onSubmit={validate(handleSubmit)}>
+      <Form.Group className="mb-3 col-4" controlId="formBasicEmail">
         <Form.Label>Title</Form.Label>
-        <Form.Control type="text"
-          placeholder="Enter title"
+        <Form.Control
+          {...register("title", { required: true, minLength: 3 })}
           value={title}
           onChange={e => setTitle(e.target.value)}
+          type="text" placeholder="Enter title"
         />
+        {errors.title && <small className="d-block form-text text-danger mt-2">Title is too short (min is 3)</small>}
       </Form.Group>
 
       <Form.Group className="mb-3 col-4" controlId="formAuthor">
         <Form.Label>Author</Form.Label>
-        <Form.Control type="text"
-          placeholder="Enter author"
+        <Form.Control
+          {...register("author", { required: true, minLength: 3 })}
           value={author}
           onChange={e => setAuthor(e.target.value)}
+          type="text" placeholder="Enter title"
         />
+        {errors.author && <small className="d-block form-text text-danger mt-2">Title is too short (min is 3)</small>}
       </Form.Group>
 
       <Form.Group className="mb-3 col-4" controlId="formPublished">
         <Form.Label>Published</Form.Label>
-        <DatePicker 
-        selected={publishedDate}
-        onChange={(date) => setPublishedDate(date)}
+        <DatePicker
+          selected={publishedDate}
+          onChange={(date) => setPublishedDate(date)}
         />
+        {dateError && <small className="d-block form-text text-danger mt-2">Date can't be empty</small>}
       </Form.Group>
 
       <Form.Group className="mb-3 col-6" controlId="formDescription">
         <Form.Label>Short description</Form.Label>
         <Form.Control
+          {...register("shortDescription", { required: true, minLength: 3 })}
           as='textarea'
           rows={4}
-          type="textarea"
-          placeholder="Leave a comment here"
           value={shortDescription}
           onChange={e => setShortDescription(e.target.value)}
+          type="textarea" placeholder="Leave a comment here"
         />
+        {errors.shortDescription && <small className="d-block form-text text-danger mt-2">Short Description is too short (min is 3)</small>}
       </Form.Group>
+
 
       <Form.Group className="mb-3 col-6" controlId="formContent">
         <Form.Label>Main content</Form.Label>
-        <ReactQuill 
-        theme="snow" 
-        value={content} 
-        onChange={setContent}
+        <ReactQuill
+          theme="snow"
+          value={content}
+          onChange={setContent}
         />
+        {contentError && <small className="d-block form-text text-danger mt-2">Content can't be empty</small>}
       </Form.Group>
       <Button variant="primary" type="submit">
         Add post
