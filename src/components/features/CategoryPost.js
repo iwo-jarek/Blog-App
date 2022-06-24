3b5be0d16f9a2f482038a015af65b7aef6fba0a5
iwona <iwo.jarek@gmail.com>
2022-06-24 21:20:28 +0200

    categories

--- src/components/features/CategoryPost.js
+++ src/components/features/CategoryPost.js
@@ -1,4 +1,4 @@
-import { Card, Button, Row, Col } from "react-bootstrap";
+import { Card, Button, Col } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getPostsByCategory } from "../../redux/postsRedux";
